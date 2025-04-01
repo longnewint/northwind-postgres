08374c919ada75c7ef2c958c0486627eaf8f61b0
Long Nguyen <longnewint@gmail.com>
2025-03-31 23:32:40 -0400

    Comm1 in data0

--- northwind-data-0.sql
+++ northwind-data-0.sql
@@ -1,6 +1,7 @@
 --
 -- Northwind database dump
 --
+-- C1
 
 INSERT INTO categories VALUES (1, 'Beverages', 'Soft drinks, coffees, teas, beers, and ales', '\x');
 INSERT INTO categories VALUES (2, 'Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings', '\x');
