527e326618067454aca99b2fe3760c424f56c1e0
Long Nguyen <longnewint@gmail.com>
2025-04-03 23:02:24 -0400

    Add cm6 in data0

--- northwind-data-0.sql
+++ northwind-data-0.sql
@@ -7,6 +7,7 @@
 -- cm3
 -- cm4
 -- cm5
+-- cm6
 
 INSERT INTO categories VALUES (1, 'Beverages', 'Soft drinks, coffees, teas, beers, and ales', '\x');
 INSERT INTO categories VALUES (2, 'Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings', '\x');
