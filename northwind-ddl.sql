f8a0826cc795b9222152c8e3bb04791df0dc5428
Long Nguyen <longnewint@gmail.com>
2025-04-10 23:45:25 -0400

    Format ddl0

--- northwind-ddl.sql
+++ northwind-ddl.sql
@@ -12,9 +12,10 @@ SET client_min_messages = warning;
 SET default_tablespace = '';
 SET default_with_oids = false;
 
----
+--
 --- drop tables
 ---
+
 
 
 DROP TABLE IF EXISTS customer_customer_demo;
