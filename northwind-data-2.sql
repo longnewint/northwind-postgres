9d97f948d41727383bff9b02fd4235cdc00b4306
Long Nguyen <longnewint@gmail.com>
2025-04-23 23:15:08 -0400

    Clean d2

--- northwind-data-2.sql
+++ northwind-data-2.sql
@@ -1,13 +1,6 @@
 --
 -- Northwind database dump
 --
--- c0
--- c1
--- c2
--- c3
--- c4
--- c5
--- c6
 
 INSERT INTO order_details VALUES (10248, 11, 14, 12, 0);
 INSERT INTO order_details VALUES (10248, 42, 9.80000019, 10, 0);
